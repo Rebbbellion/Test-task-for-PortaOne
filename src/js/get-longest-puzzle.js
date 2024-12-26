c3b545f1f159adad98473021126ae251e6a2f4e8
Maksym Dzhosan <dzhomaks@gmail.com>
2024-12-26 02:41:00 +0200

    fix: used recursion to find the longest puzzle
    - switched from iterative method to recursive to get better result

--- src/js/get-longest-puzzle.js
+++ src/js/get-longest-puzzle.js
@@ -1,10 +1,9 @@
 function findTheLongestPuzzle(puzzlesArray) {
   const puzzlesGraph = mapArrayToGraph(puzzlesArray);
-  const memory = new Map();
   let longestPuzzle = "";
 
   for (const puzzle of puzzlesArray) {
-    const currentPuzzle = gatherPuzzle(puzzle, puzzlesGraph, memory);
+    const currentPuzzle = gatherPuzzle(puzzle, puzzlesGraph);
 
     if (currentPuzzle.length > longestPuzzle.length) {
       longestPuzzle = currentPuzzle;
@@ -18,7 +17,7 @@ function mapArrayToGraph(array) {
   const hashTable = new Map();
   const graph = {};
 
-  //Group puzzles by first two digits
+  // Group puzzles by first two digits
   for (const puzzle of array) {
     const key = puzzle.slice(0, 2);
     if (!hashTable.has(key)) {
@@ -27,7 +26,7 @@ function mapArrayToGraph(array) {
     hashTable.get(key).push(puzzle);
   }
 
-  //Build the graph using the last two digits as a key
+  // Build the graph using the last two digits as a key
   for (const puzzle of array) {
     const key = puzzle.slice(-2);
     graph[puzzle] = hashTable.get(key) || [];
@@ -36,59 +35,32 @@ function mapArrayToGraph(array) {
   return graph;
 }
 
-function gatherPuzzle(puzzle, graph, memory) {
-  const stack = [{ puzzle, gatheredPuzzle: puzzle }];
-  let longestPuzzle = puzzle;
-  const usedPuzzles = new Set();
-  const parentsPath = {};
+function gatherPuzzle(
+  puzzle,
+  graph,
+  gatheredPuzzle = puzzle,
+  usedPuzzles = new Set()
+) {
+  usedPuzzles.add(puzzle);
 
-  while (stack.length > 0) {
-    const { puzzle: currentPuzzle, gatheredPuzzle } = stack.pop();
+  let longestPuzzle = gatheredPuzzle;
 
-    //Using memory if we've already gathered puzzle earlier
-    if (memory.has(currentPuzzle)) {
-      const combinedPuzzle =
-        gatheredPuzzle + memory.get(currentPuzzle).slice(6);
-      //Removing repeating part of puzzle in memory which already exists in gatheredPuzzle
-
-      if (combinedPuzzle.length > longestPuzzle.length) {
-        longestPuzzle = combinedPuzzle;
-      }
-      continue;
-    }
-
-    if (gatheredPuzzle.length > longestPuzzle.length) {
-      longestPuzzle = gatheredPuzzle;
-    }
-
-    for (const childPuzzle of graph[currentPuzzle]) {
-      if (!usedPuzzles.has(childPuzzle)) {
-        stack.push({
-          puzzle: childPuzzle,
-          gatheredPuzzle: gatheredPuzzle + childPuzzle.slice(2), // Skip first two digits to concatenate the puzzle
-        });
-        parentsPath[childPuzzle] = currentPuzzle; // Creating parent-child relationship to fill up memory later
+  for (const childPuzzle of graph[puzzle]) {
+    if (!usedPuzzles.has(childPuzzle)) {
+      const combinedPuzzle = gatheredPuzzle + childPuzzle.slice(2);
+      const result = gatherPuzzle(
+        childPuzzle,
+        graph,
+        combinedPuzzle,
+        usedPuzzles
+      );
+      if (result.length > longestPuzzle.length) {
+        longestPuzzle = result;
       }
     }
-
-    usedPuzzles.add(currentPuzzle);
   }
 
-  //Filling up memory using existing parent—child relationships
-  fillUpMemory(parentsPath, memory);
-
-  memory.set(puzzle, longestPuzzle);
-
   return longestPuzzle;
-}
-
-function fillUpMemory(parentsPath, memory) {
-  Object.entries(parentsPath).forEach(([childKey, parentValue]) => {
-    let combinedPuzzle = memory.get(childKey)
-      ? parentValue + memory.get(childKey).slice(2)
-      : parentValue + childKey.slice(2);
-    memory.set(parentValue, combinedPuzzle);
-  });
 }
 
 export { findTheLongestPuzzle };
